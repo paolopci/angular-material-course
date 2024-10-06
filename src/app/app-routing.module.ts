59d67f6b55a21e3756dbee5382fdce674c4ab7cd
PaoloPci <paolopci@yahoo.it>
2024-10-06 09:31:11 +0200

    add initial create a new matDialog component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {CourseComponent} from "./course/course.component";
-import {courseResolver} from "./services/course.resolver";
-import {CreateCourseComponent} from './create-course/create-course.component';
-import {DragDropComponent} from './drag-drop/drag-drop.component';
-import {TreeDemoComponent} from './tree-demo/tree-demo.component';
-import {VirtualScrollingComponent} from './virtual-scrolling/virtual-scrolling.component';
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { CourseComponent } from "./course/course.component";
+import { courseResolver } from "./services/course.resolver";
+import { CreateCourseComponent } from './create-course/create-course.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { TreeDemoComponent } from './tree-demo/tree-demo.component';
+import { VirtualScrollingComponent } from './virtual-scrolling/virtual-scrolling.component';
 
 const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
-
-    },
-    {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-        path: 'courses/:id',
-        component: CourseComponent,
-        resolve: {
-            course: courseResolver
-        }
-    },
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: 'courses/:id',
+    component: CourseComponent,
+    resolve: {
+      course: courseResolver
+    }
+  },
   {
     path: 'add-new-course',
     component: CreateCourseComponent
@@ -42,10 +41,10 @@ const routes: Routes = [
     path: 'virtual-scrolling',
     component: VirtualScrollingComponent
   },
-    {
-        path: "**",
-        redirectTo: '/'
-    }
+  {
+    path: "**",
+    redirectTo: '/'
+  }
 ];
 
 @NgModule({
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,7 +1,7 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA,  MatDialog,  MatDialogConfig,  MatDialogRef} from '@angular/material/dialog';
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from 'moment';
 
 @Component({
@@ -11,14 +11,23 @@ import * as moment from 'moment';
 })
 export class CourseDialogComponent implements OnInit {
 
+    description: string = '';
+    form = this.fb.group({
+        description: ['', Validators.required],
+        category: ['BEGINNER', Validators.required],
+        releasedAt: [new Date(), Validators.required],
+        longDescription: ['', Validators.required]
+    });
+
     constructor(private fb: FormBuilder) {
-
-
     }
 
     ngOnInit() {
-
     }
+
+    close() { }
+
+    save() { }
 
 }
 
